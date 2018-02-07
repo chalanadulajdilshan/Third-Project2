0ecef058d4688936f659c65fb8fd9fead5c543e5
chalanadulaj <chalanadulaj99@gmail.com>
2018-02-07 14:33:03 +0530

    Add Footer In Index Page

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html lang="en">
     <head>
-     <meta charset="utf-8">
+        <meta charset="utf-8">
 
         <title>HTML5 Index Page</title>
         <meta name="description" content="This is an HTML5 file. This description text must be changed for SEO purpose">
@@ -17,8 +17,8 @@
         <![endif]-->
     </head>
     <body>
-       
-        
+
+
         <div class="container-fluid">
             <div class="row" id="header1">
                 <div class="container header inner">
@@ -48,8 +48,8 @@
         <br/>
         <br/>
         <br/>
-        
-          <div class="container-fluid">
+
+        <div class="container-fluid">
             <div class="row">
                 <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
                     <!-- Indicators -->
@@ -101,7 +101,7 @@
         <br/>
         <br/>
 
- <div class="container">
+        <div class="container">
             <div class="row">
                 <div class="col-lg-6">
                     <img src="images/darkk5.png" alt="" class="img-responsive" id="image2" />
@@ -125,55 +125,44 @@
 
                 </div>
             </div>
+
+
+            <br/>        
+            <br/>        
+            <br/>
+            <div class="row">
+                <div class="col-md-3 col-lg-3">
+                    <img src="images/dark1.jpg" alt="" id="image3"/>
+                </div>
+                <div class="col-md-3 col-lg-3">
+                    <img src="images/dark2.jpg" alt="" id="image3"/>
+                </div>
+                <div class="col-md-3 col-lg-3">
+                    <img src="images/dark3.jpg" alt="" id="image3"/>
+                </div>
+                <div class="col-md-3 col-lg-3">
+                    <img src="images/dark4.jpg" alt="" id="image3"/>
+                </div>
+
             </div>
 
-        <br/>        
-        <br/>        
-        <br/>
-        
-        
-         <div class="row">
-            
-             <div class="container">
-                <h1>Use Bootstrap's carousel to show multiple items per slide.</h1>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="carousel slide multi-item-carousel" id="theCarousel">
-                            <div class="carousel-inner">
-                                <div class="item active">
-                                    <div class="col-xs-4"><a href="#1"><img src="images/image1.jpg" class="img-responsive"></a></div>
-                                </div>
-                                <div class="item">
-                                    <div class="col-xs-4"><a href="#1"><img src="images/image2.jpg" class="img-responsive"></a></div>
-                                </div>
-                                <div class="item">
-                                    <div class="col-xs-4"><a href="#1"><img src="images/image3.jpg" class="img-responsive"></a></div>
-                                </div>
-                                <div class="item">
-                                    <div class="col-xs-4"><a href="#1"><img src="images/image4.jpg" class="img-responsive"></a></div>
-                                </div>
-                                <div class="item">
-                                    <div class="col-xs-4"><a href="#1"><img src="images/image5.jpg" class="img-responsive"></a></div>
-                                </div>
-                                <div class="item">
-                                    <div class="col-xs-4"><a href="#1"><img src="images/image6.jpg" class="img-responsive"></a></div>
-                                </div>
-                                <!-- add  more items here -->
-                                <!-- Example item start:  -->
 
-                                <div class="item">
-                                    <div class="col-xs-4"><a href="#1"><img src="images/image7.jpg" class="img-responsive"></a></div>
-                                </div>
-
-                                <!--  Example item end -->
-                            </div>
-                            <a class="left carousel-control" href="#theCarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
-                            <a class="right carousel-control" href="#theCarousel" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
-                        </div>
-                    </div>
+            <br/>
+            <br/>
+            <br/>
+        </div>
+        <div class="row" style="background:#74bbb3;" >
+            <div class="col-md-12 col-lg-12 ">
+                <div class="text-center center-block">
+                    <p class="txt-railway"><h1><b>- Footter-</b></h1></p>
+                    <br />
+                    <a href=""><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
+                    <a href=""><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
+                    <a href=""><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
+                    <a href=""<i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
                 </div>
             </div>
         </div>
-        
+
     </body>
 </html>
