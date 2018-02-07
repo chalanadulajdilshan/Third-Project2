49da46d0dd73fca2b61df9a9b64ecc9ab2469841
chalanadulaj <chalanadulaj99@gmail.com>
2018-02-07 13:43:14 +0530

    Add Left Side Image And Right Side Descrption

--- index.php
+++ index.php
@@ -94,6 +94,40 @@
                 </div>
             </div>
         </div>
+        <br/>
+        <br/>
+        <br/>
+
+ <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <img src="images/darkk5.png" alt="" class="img-responsive" id="image2" />
+
+                </div>
+                <div class="col-lg-6">
+                    <h3 class="text-center"><b>About As..!</b> </h3>
+                    <p class="text-justify">It is a long established fact that a reader will be distracted
+                        by the readable content of a page when looking at its layout. The point of using Lorem 
+                        Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 
+                        'Content here, content here', making it look like readable English. Many desktop publishing
+                        packages and web page editors now use Lorem Ipsum as their default model text, and a search for 
+                        'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved
+                        over the years, sometimes by accident, sometimes on purpose (injected humour and the like) a reader will be distracted
+                        by the readable content of a page when looking at its layout. The point of using Lorem 
+                        Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 
+                        'Content here, content here', making it look like readable English. Many desktop publishing
+                        packages and web page editors now use Lorem Ipsum as their default model text, and a search for 
+                        'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved.</p>
+
+
+                </div>
+            </div>
+            </div>
+
+        <br/>        
+        <br/>        
+        <br/>
+        
         
         
         
