74324ca4441edee14b79e938587bbb0783a9a17d
chalanadulaj <chalanadulaj99@gmail.com>
2018-02-07 12:52:48 +0530

    Create Basic Tags In Page

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
-<html>
+<html lang="en">
     <head>
-        <title>
-            My First web Page
-        </title>
+        <meta charset="utf-8">
+
+        <title>HTML5 Index Page</title>
+        <meta name="description" content="This is an HTML5 file. This description text must be changed for SEO purpose">
+
+        <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="style.css">
+
+        <script src="vendor/jquery-2.2.4.min.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+        <!--[if lt IE 9]>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+        <![endif]-->
     </head>
     <body>
-        <p>
-            Learn the details
-        </p>
+       
+        
+        
+        
+        
+        
     </body>
 </html>
