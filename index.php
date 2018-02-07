cc9aad75a8c55055235d2b32545a72db86b9ee5a
chalanadulaj <chalanadulaj99@gmail.com>
2018-02-07 12:55:17 +0530

    Create Top Header,Add Logo Image And Navigation Bar

--- index.php
+++ index.php
@@ -19,6 +19,32 @@
     <body>
        
         
+        <div class="container-fluid">
+            <div class="row" id="header1">
+                <div class="container header inner">
+                    <nav class="navbar">
+                        <div class="container">
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                               
+                            </div>
+                            <div class="collapse navbar-collapse" id="myNavbar">
+                                <ul class="nav navbar-nav navbar-right masthead-nav">
+                                    <li><a href="#"> E-commerce Logistics</a></li>
+                                    <li><a href="#"> Contract Logistics</a></li>
+                                    <li><a href="#"> Courier</a></li>
+                                    <li><a href="#"> Transport</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </div> 
+            </div> 
+        </div> 
         
         
         
